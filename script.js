fb2d230f8582b99b18e7e4246967dfc146f0467a
@amritmohan <amritmohan2284@gmail.com>
2025-02-06 11:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
-	let newStr = str.trim();
+	let newStr = s.trim();
     let index = newStr.indexOf(" ");
     return index === -1 ? newStr: newStr.substring(0, index);
+	
 }
 
 // Do not change the code below
